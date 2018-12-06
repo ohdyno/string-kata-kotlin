5cc8ba4fb9888ec824c46a97466d6aacbcc38243
xing <xing@integral.io>
2018-12-06 09:49:21 -0500

    Adding empty string returns 0

--- src/test/kotlin/CalculatorTests.kt
+++ src/test/kotlin/CalculatorTests.kt
@@ -1,7 +1,13 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class CalculatorTests {
     @Test
-    fun `verify that we can write a Kotlin test`() {
+    fun `adding empty string returns 0`() {
+        assertEquals(0, Calculator().add(""))
     }
+}
+
+class Calculator {
+    fun add(numbers: String): Int = 0
 }
